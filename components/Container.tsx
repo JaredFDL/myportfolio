4a391ca17df1df077fea7e5ecdd35cb6880f6d0d
Jared Feng <fengdongliang002@gmail.com>
2024-11-11 19:10:14 +1000

    container modified

--- components/Container.tsx
+++ components/Container.tsx
@@ -8,12 +8,13 @@ function Container({
 }: React.HTMLAttributes<HTMLElement>) {
   return (
     <section
-      className={twMerge("w-full bg-muted py-16 md:py-20", className)}
+      className={twMerge(
+        "w-full py-16 mx-auto flex max-w-7xl flex-col gap-6 px-8",
+        className
+      )}
       {...props}
     >
-      <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 px-4 md:gap-12 md:px8">
-        {children}
-      </div>
+      {children}
     </section>
   );
 }
