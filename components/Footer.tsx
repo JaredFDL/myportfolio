317d2f8df821e64fe88ce32980780c6b22e194c4
Jared Feng <fengdongliang002@gmail.com>
2024-11-12 13:42:01 +1000

    Update footer margin

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import { Copyright } from "lucide-react";
 
 function Footer() {
   return (
-    <footer className="w-full mt-3 mb-1 mx-auto">
+    <footer className="w-full mt-4 mb-3 mx-auto">
       <p className="flex flex-row justify-center text-center">
         <Copyright className="w-[18px] h-[18px] mr-1" />
         {new Date().getFullYear()} | Designed and coded by Jared Feng with 💗
